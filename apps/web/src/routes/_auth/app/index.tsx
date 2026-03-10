5f75bf0c894fd789e723cde1d5acc8756b95483c
Nathaniel John Tampus <nat@nize.ph>
2026-03-10 14:44:47 +0800

    feat: improve /app page

--- apps/web/src/routes/_auth/app/index.tsx
+++ apps/web/src/routes/_auth/app/index.tsx
@@ -1,29 +1,31 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { SignOutButton } from "~/components/sign-out-button";
-
 export const Route = createFileRoute("/_auth/app/")({
   component: AppIndex,
 });
 
 function AppIndex() {
   const { user } = Route.useRouteContext();
-  // we can also use the useAuth() or useAuthSuspense() hooks here from @repo/auth/tanstack/hooks
+  // we can also use the useAuth() or useAuthSuspense() hooks here from ~/lib/auth/hooks
   // this is just to demo that route context is available in route components, in addition to loaders/beforeLoad
 
   return (
-    <div className="flex flex-col items-center gap-1">
-      App index page
-      <pre className="rounded-md border bg-card p-1 text-xs text-card-foreground">
-        routes/_auth/app/index.tsx
+    <div className="flex flex-col items-center gap-3 text-center text-sm">
+      <pre className="mb-1 rounded-md border bg-card p-1 text-xs text-card-foreground">
+        _auth/app/index.tsx
       </pre>
-      <div className="mt-2 text-center text-xs sm:text-sm">
-        User data from route context:
-        <pre className="max-w-screen overflow-x-auto px-2 text-start">
-          {JSON.stringify(user, null, 2)}
+
+      <div>
+        User from route context:
+        <span className="mt-0.5 block font-mono text-xs">{user.name}</span>
+      </div>
+
+      <div>
+        <p>The /app index page, a protected route, since it is under the _auth layout:</p>
+        <pre className="mx-auto mt-0.5 block w-fit rounded-md border bg-card p-1 text-xs text-card-foreground">
+          _auth/route.tsx
         </pre>
       </div>
-      <SignOutButton />
     </div>
   );
 }
--- apps/web/src/routes/_auth/app/route.tsx
+++ apps/web/src/routes/_auth/app/route.tsx
@@ -1,31 +1,40 @@
 import { Button } from "@repo/ui/components/button";
 import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
+import { SignOutButton } from "~/components/sign-out-button";
+import { ThemeToggle } from "~/components/theme-toggle";
+
 export const Route = createFileRoute("/_auth/app")({
   component: AppLayout,
 });
 
 function AppLayout() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-10 p-2">
-      <div className="flex flex-col items-center gap-2">
-        <h1 className="text-3xl font-bold sm:text-4xl">App Layout</h1>
-        <pre className="mb-4 rounded-md border bg-card p-1 text-xs text-card-foreground">
-          routes/_auth/app/route.tsx
-        </pre>
-        <div className="mb-4 flex flex-col items-center gap-2 text-sm text-foreground/80">
-          This is a protected layout from the _auth pathless layout route:
-          <pre className="rounded-md border bg-card p-1 text-xs text-card-foreground">
-            routes/_auth/route.tsx
-          </pre>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-2 px-2">
+      <div className="flex w-full max-w-3xl justify-between">
+        <div className="flex items-center gap-1">
+          <Button render={<Link to="/" />} size="sm" nativeButton={false}>
+            back to home
+          </Button>
+          <span className="rounded-md border bg-card p-1 font-mono text-xs text-card-foreground">
+            /_auth/app/route.tsx
+          </span>
         </div>
-
-        <Button render={<Link to="/" />} className="w-fit" size="lg" nativeButton={false}>
-          Back to home
-        </Button>
+        <ThemeToggle />
+      </div>
+      <div className="w-full max-w-3xl rounded-md border p-2">
+        <Outlet />
       </div>
 
-      <Outlet />
+      <div className="flex w-full max-w-3xl flex-wrap justify-between gap-2 text-sm">
+        <div className="flex flex-col gap-0.5">
+          what's next? maybe a sidebar?
+          <span className="rounded-md border bg-card px-2 py-1 font-mono text-xs text-card-foreground">
+            pnpm ui add sidebar
+          </span>
+        </div>
+        <SignOutButton />
+      </div>
     </div>
   );
 }
