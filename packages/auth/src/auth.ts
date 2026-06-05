40751d65d2f89ddfbf240ad19450c98f231dd2a8
Nathaniel John Tampus <nate@mugnavo.com>
2026-06-05 21:15:26 +0800

    chore: switch to vendored better-auth drizzle adapter package

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,5 +1,6 @@
 import "@tanstack/react-start/server-only";
-import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+// import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { db } from "@repo/db";
 import * as schema from "@repo/db/schema";
 import { betterAuth } from "better-auth/minimal";
